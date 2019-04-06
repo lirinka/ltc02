c8ff0ed5e45822f63c9cdcbfaf8b50c928c2e0bc
Your Name <you@example.com>
2019-04-06 11:49:08 +0300

    sum

---
+++ sum.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+int sum(int b, int c) {
+    return b+c;
+}
+int main() {
+    printf("%d",sum(2,3));
+}
---
+++ sum.js
@@ -0,0 +1,6 @@
+let a = sum (2,3)
+function sum(b,c)
+{
+return(b+c);
+}
+console.log(a)
